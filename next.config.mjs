e4e7f2fbde6e99c3d5fe7f321f3cff03dc07e704
Jorge Calleja <posicionadoenlaweb@gmail.com>
2026-01-07 11:18:28 +0100

    feat: add redirect from smart-contracts-101 to smart-contracts

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:lng/learn/smart-contracts-101',
+        destination: '/:lng/learn/smart-contracts',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
