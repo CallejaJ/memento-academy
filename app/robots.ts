21fb5d008c751c766048fa87409a11f88ed3e29c
Jorge Calleja <posicionadoenlaweb@gmail.com>
2026-01-18 15:23:04 +0100

    fix: add sitemap-twitter to robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,15 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/private/", "/admin/"],
     },
-    sitemap: 'https://memento-academy.com/sitemap.xml', // Replace with actual domain
-  }
+    sitemap: [
+      "https://memento-academy.com/sitemap.xml",
+      "https://memento-academy.com/sitemap-twitter.xml",
+    ],
+  };
 }
