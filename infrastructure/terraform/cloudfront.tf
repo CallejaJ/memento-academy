82d0218555673d15e8cd1db48ee3186527b4f9ac
Jorge Calleja <posicionadoenlaweb@gmail.com>
2026-04-02 23:06:15 +0200

    fix: remove invalid custom_error_response fields for CloudFront

--- infrastructure/terraform/cloudfront.tf
+++ infrastructure/terraform/cloudfront.tf
@@ -44,8 +44,6 @@ resource "aws_cloudfront_distribution" "assets" {
 
   custom_error_response {
     error_code            = 403
-    response_code         = 404
-    response_page_path    = ""
     error_caching_min_ttl = 300
   }
 }
